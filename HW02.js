b598d0f27894788d13b9f245dd1def6a8b47a3f8
EJALLAH <JALLAEF23@juniata.edu>
2024-09-18 20:32:35 -0400

    new

--- HW02.js
+++ HW02.js
@@ -1,14 +1,33 @@
 Function sierpinski()
 {
-  initialize_the_system();
-  P = find_initial_point();
+initialize_the_system();
+p = find_initial_point();
 
-For (some_number_of_points)
+for (some_number_of_points)
 {
 q = generate_a_point(p);
-display_the_point(q);
+store_the_point(q);
 p = q;
 }
+display_all_points();
 cleanup();
+
+var u = mult(0.5, add(vertices[0], vertices[1]));
+var v = mult(0.5, add(vertices[0], vertices[2]));
+var p = mult(0.5, add(u, v));
+
+Positions.push(p);
+
+for (var i = 0; i < numPosition - 1; ++i)
+{
+var j = Math.floor(Math.random() * 3);
+
+p = mult(0.5, add(position[I], vertices[j]));
+positions.push(p);
+
 }
+
+
+
+
 	
