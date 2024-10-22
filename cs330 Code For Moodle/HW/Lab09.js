170cb4c31a6abee5f7ca28d1c0ede57b149c53ac
EJALLAH <JALLAEF23@juniata.edu>
2024-10-22 10:06:07 -0400

    new

--- cs330 Code For Moodle/HW/Lab09.js
+++ cs330 Code For Moodle/HW/Lab09.js
@@ -1,75 +1,123 @@
-<!DOCTYPE html>
-<html>
-<HEAD>
-<meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
-<title>Lab 09 Color Cube</title>
-</HEAD>
-<body>
+"use strict";
 
-<canvas id="gl-canvas" width="512"" height="512"> </canvas>
+var canvas;
+var gl;
 
-<button id= "xButton">Rotate X</button>
-<button id= "yButton">Rotate Y</button>
-<button id= "zButton">Rotate Z</button>
-<button id = "ButtonT">Toggle Rotation</button>
-<script id="vertex-shader" type="x-shader/x-vertex">
-#version 300 es
+var axis = 0;
+var xAxis = 0;
+var yAxis =1;
+var zAxis = 2;
+var theta = [0, 0, 0];
+var thetaLoc;
+var flag = true;
+var numElements = 29;
 
-in  vec4 aPosition;
-in  vec4 aColor;
-out vec4 vColor;
+    var vertices = [
+        vec3(-0.5, -0.5,  0.5),
+        vec3(-0.5,  0.5,  0.5),
+        vec3(0.5,  0.5,  0.5),
+        vec3(0.5, -0.5,  0.5),
+        vec3(-0.5, -0.5, -0.5),
+        vec3(-0.5,  0.5, -0.5),
+        vec3(0.5,  0.5, -0.5),
+        vec3(0.5, -0.5, -0.5)
+    ];
 
-uniform vec3 uTheta;
+    var vertexColors = [
+        vec4(0.0, 0.0, 0.0, 1.0),  // black
+        vec4(1.0, 0.0, 0.0, 1.0),  // red
+        vec4(1.0, 1.0, 0.0, 1.0),  // yellow
+        vec4(0.0, 1.0, 0.0, 1.0),  // green
+        vec4(0.0, 0.0, 1.0, 1.0),  // blue
+        vec4(1.0, 0.0, 1.0, 1.0),  // magenta
+        vec4(1.0, 1.0, 1.0, 1.0),  // white
+        vec4(0.0, 1.0, 1.0, 1.0)   // cyan
+    ];
 
-void main()
+// indices of the 12 triangles that compise the cube
+
+var indices = [
+    1, 0, 3, 2, 255,
+    2, 3, 7, 2, 255,
+    3, 0, 4, 7, 255,
+    6, 5, 1, 2, 255,
+    4, 5, 6, 7, 255,
+    5, 4, 0, 1
+];
+
+init();
+
+function init()
 {
-    // Compute the sines and cosines of theta for each of
-    //   the three axes in one computation.
-    vec3 angles = radians( uTheta );
-    vec3 c = cos( angles );
-    vec3 s = sin( angles );
+    canvas = document.getElementById("gl-canvas");
 
-    // Remeber: thse matrices are column-major
-    mat4 rx = mat4( 1.0,  0.0,  0.0, 0.0,
-		    0.0,  c.x,  s.x, 0.0,
-		    0.0, -s.x,  c.x, 0.0,
-		    0.0,  0.0,  0.0, 1.0 );
-
-    mat4 ry = mat4( c.y, 0.0, -s.y, 0.0,
-		    0.0, 1.0,  0.0, 0.0,
-		    s.y, 0.0,  c.y, 0.0,
-		    0.0, 0.0,  0.0, 1.0 );
+    gl = canvas.getContext('webgl2');
+    if (!gl) alert("WebGL 2.0 isn't available");
 
 
-    mat4 rz = mat4( c.z, s.z, 0.0, 0.0,
-		    -s.z,  c.z, 0.0, 0.0,
-		    0.0,  0.0, 1.0, 0.0,
-		    0.0,  0.0, 0.0, 1.0 );
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
 
-    vColor = aColor;
-    gl_Position = rz * ry * rx * aPosition;
-    gl_Position.z = -gl_Position.z;
+    gl.enable(gl.DEPTH_TEST);
+    gl.enable(gl.PRIMITIVE_RESTART_FIXED_INDEX);
+
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    var program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
+
+    // array element buffer
+
+    var iBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, iBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint8Array(indices), gl.STATIC_DRAW);
+
+    // color array atrribute buffer
+
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertexColors), gl.STATIC_DRAW);
+
+    var colorLoc = gl.getAttribLocation(program, "aColor");
+    gl.vertexAttribPointer(colorLoc, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(colorLoc);
+
+    // vertex array attribute buffer
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
+
+    var positionLoc = gl.getAttribLocation( program, "aPosition");
+    gl.vertexAttribPointer(positionLoc, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(positionLoc );
+
+    thetaLoc = gl.getUniformLocation(program, "uTheta");
+
+    //event listeners for buttons
+
+    document.getElementById( "xButton" ).onclick = function () {
+        axis = xAxis;
+    };
+    document.getElementById( "yButton" ).onclick = function () {
+        axis = yAxis;
+    };
+    document.getElementById( "zButton" ).onclick = function () {
+        axis = zAxis;
+    };
+    document.getElementById("ButtonT").onclick = function(){flag = !flag;};
+
+    render();
 }
-</script>
 
-<script id="fragment-shader" type="x-shader/x-fragment">
-#version 300 es
-
-precision mediump float;
-
-in vec4 vColor;
-out vec4 fColor;
-
-void
-main()
+function render()
 {
-    fColor = vColor;
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    if(flag) theta[axis] += 2.0;
+    gl.uniform3fv(thetaLoc, theta);
+
+    gl.drawElements(gl.TRIANGLE_FAN, numElements, gl.UNSIGNED_BYTE, 0);
+    requestAnimationFrame(render);
 }
-</script>
-
-<script src="../Common/initShaders.js"></script>
-<script src="../Common/MVnew.js"></script>
-<script src="Lab09.js"></script>
-
-</body>
-</html>
