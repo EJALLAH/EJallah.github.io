f8ac3645a9b4c9bc8b35067a39dd386f6582863a
EJALLAH <JALLAEF23@juniata.edu>
2024-10-23 20:08:06 -0400

    Update Lab10.js

--- cs330 Code For Moodle/HW/Lab10.js
+++ cs330 Code For Moodle/HW/Lab10.js
@@ -134,7 +134,10 @@ function render()
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     theta[axis] += 2.0;
-    gl.uniform3fv(thetaLoc, theta);
+    gl.uniform3fv(thetaLoc, theta)
+
+    // Using the key to for the axes
+    gl.drawArrays(gl.LINES, numPositions, 6);
 
     gl.drawArrays(gl.TRIANGLES, 0, numPositions);
     requestAnimationFrame(render);
